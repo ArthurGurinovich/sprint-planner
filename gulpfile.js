d69487620fdca6eeb5057b8acdc2145b8ca23692
ArthurGurinovich <gurinovichag@gmail.com>
2017-10-23 21:52:19 +0300

    Header and Footer

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var concatCss = require('gulp-concat-css');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var uncss = require('gulp-uncss');
+var clean = require('gulp-clean');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('css', function () {
+  return gulp.src('css/**/*.css')
+    .pipe(concatCss('style.css'))
+    .pipe(autoprefixer({
+            browsers: ['last 5 versions'],
+            cascade: false
+        }))
+    .pipe(cleanCSS())
+    .pipe(uncss({
+        html: ['./app/index.html', './app/users/*.html']
+    }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('app/css/'));
+});
+
+gulp.task('clean', function () {
+  return gulp.src(['./css/*.css', './app/css/*.css'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./css/**/*.css', ['css']);
+});
